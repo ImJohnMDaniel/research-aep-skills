6e7981c40c41d335bec839c9a1455f0568763091
John M. Daniel <john.m.daniel@associates.cbp.dhs.gov>
2026-06-19 20:11:10 -0400

    fix: syntax corrections to script

--- skills/manage-apex-selectors/scripts/create_selector.cjs
+++ skills/manage-apex-selectors/scripts/create_selector.cjs
@@ -48,7 +48,7 @@ function isSupportedByMetadataRelationship(name) {
 function updateFile(filePath, content, templateIfMissing) {
     if (!fs.existsSync(filePath)) {
         fs.writeFileSync(filePath, templateIfMissing);
-        console.log(\ - Created: \\);
+        console.log(` - Created: ${filePath}`);
         return true;
     }
     let existingContent = fs.readFileSync(filePath, "utf8");
@@ -60,25 +60,25 @@ function updateFile(filePath, content, templateIfMissing) {
             const updatedContent = existingContent.replace(fieldListRegex, content.trim());
             if (updatedContent !== existingContent) {
                 fs.writeFileSync(filePath, updatedContent);
-                console.log(\ - Updated field list: \\);
+                console.log(` - Updated field list: ${filePath}`);
                 return true;
             }
-            console.log(\ - Field list up to date: \\);
+            console.log(` - Field list up to date: ${filePath}`);
             return false;
         }
     }
 
     if (existingContent.includes(content.trim())) {
-        console.log(\ - Up to date: \\);
+        console.log(` - Up to date: ${filePath}`);
         return false;
     }
 
     if (filePath.endsWith(".cls")) {
         const lastBraceIndex = existingContent.lastIndexOf("}");
         if (lastBraceIndex !== -1) {
-            const updated = existingContent.substring(0, lastBraceIndex) + "\\n" + content + "\\n" + existingContent.substring(lastBraceIndex);
+            const updated = existingContent.substring(0, lastBraceIndex) + "\n" + content + "\n" + existingContent.substring(lastBraceIndex);
             fs.writeFileSync(filePath, updated);
-            console.log(\ - Updated: \\);
+            console.log(` - Updated: ${filePath}`);
             return true;
         }
     }
@@ -93,16 +93,16 @@ async function run() {
         const baseSanitized = sanitizeName(sObjectName, appPrefix);
         const pluralSanitized = getPlural(baseSanitized);
 
-        const className = enforceLimit(\\_\Selector\);
-        const interfaceName = enforceLimit(\\_I\Selector\);
-        const testClassName = enforceLimit(\\_\Selector\, "Test");
-        const bindingFileName = \ApplicationFactory_SelectorBinding.\_\.md-meta.xml\;
+        const className = enforceLimit(`${appPrefix}_${pluralSanitized}Selector`);
+        const interfaceName = enforceLimit(`${appPrefix}_I${pluralSanitized}Selector`);
+        const testClassName = enforceLimit(`${appPrefix}_${pluralSanitized}Selector`, "Test");
+        const bindingFileName = `ApplicationFactory_SelectorBinding.${appPrefix}_${baseSanitized}.md-meta.xml`;
 
         const paths = {
-            selector: path.join(defaultDir, "main", "classes", "selectors", \\.cls\),
-            interface: path.join(defaultDir, "main", "classes", "selectors", \\.cls\),
+            selector: path.join(defaultDir, "main", "classes", "selectors", `${className}.cls`),
+            interface: path.join(defaultDir, "main", "classes", "selectors", `${interfaceName}.cls`),
             binding: path.join(defaultDir, "main", "schema", "customMetadata", "applicationFactoryBindings", "selectorBindings", bindingFileName),
-            test: path.join(defaultDir, "test", "classes", "selectors", \\.cls\)
+            test: path.join(defaultDir, "test", "classes", "selectors", `${testClassName}.cls`)
         };
 
         Object.values(paths).forEach(p => {
@@ -110,22 +110,22 @@ async function run() {
             if (!fs.existsSync(dir)) fs.mkdirSync(dir, { recursive: true });
         });
 
-        const newInstanceBlock = \    public static \ newInstance()\\n    {\\n        return (\) Application.Selector.newInstance(\.SObjectType);\\n    }\;
+        const newInstanceBlock = `    public static ${interfaceName} newInstance()\n    {\n        return (${interfaceName}) Application.Selector.newInstance(${sObjectName}.SObjectType);\n    }`;
 
-        const fieldList = explicitFields ? explicitFields.map(f => \            \.\\).join(",\\n") : \            \.Id,\\n            \.Name\;
+        const fieldList = explicitFields ? explicitFields.map(f => `            ${sObjectName}.${f}`).join(",\n") : `            ${sObjectName}.Id,\n            ${sObjectName}.Name`;
         
-        const fieldListBlock = \    public List<Schema.SObjectField> getSObjectFieldList()\\n    {\\n        return new List<Schema.SObjectField> {\\n\\\n        };\\n    }\;
+        const fieldListBlock = `    public List<Schema.SObjectField> getSObjectFieldList()\n    {\n        return new List<Schema.SObjectField> {\n${fieldList}\n        };\n    }`;
 
         const supportsMR = isSupportedByMetadataRelationship(sObjectName);
-        const bindingSObjectValue = supportsMR ? \<value xsi:type="xsd:string">\</value>\ : \<value xsi:nil="true"/>\;
-        const bindingSObjectAlternateValue = supportsMR ? \<value xsi:nil="true"/>\ : \<value xsi:type="xsd:string">\</value>\;
+        const bindingSObjectValue = supportsMR ? `<value xsi:type="xsd:string">${sObjectName}</value>` : `<value xsi:nil="true"/>`;
+        const bindingSObjectAlternateValue = supportsMR ? `<value xsi:nil="true"/>` : `<value xsi:type="xsd:string">${sObjectName}</value>`;
 
-        const selectorTemplate = \public inherited sharing class \ \\n    extends ApplicationSObjectSelector \\n    implements \ \\n{\\n\\\n\\n\\\n\\n    public Schema.SObjectType getSObjectType()\\n    {\\n        return \.SObjectType;\\n    }\\n\\n    public List<\> selectById(Set<Id> idSet)\\n    {\\n        return (List<\>) selectSObjectsById(idSet);\\n    }\\n}\;
-        const interfaceTemplate = \public interface \ \\n    extends IApplicationSObjectSelector \\n{\\n    List<\> selectById(Set<Id> idSet);\\n}\;
-        const testTemplate = \@IsTest\\nprivate class \ \\n{\\n    @IsTest\\n    private static void testSelectById()\\n    {\\n        Id recordId = fflib_IDGenerator.generate(\.SObjectType);\\n        \.newInstance().selectById(new Set<Id> { recordId });\\n    }\\n}\;
-        const bindingTemplate = \<?xml version="1.0" encoding="UTF-8"?>\\n<CustomMetadata xmlns="http://soap.sforce.com/2006/04/metadata" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">\\n    <label>\</label>\\n    <protected>false</protected>\\n    <values>\\n        <field>BindingSObject__c</field>\\n        \\\n    </values>\\n    <values>\\n        <field>BindingSObjectAlternate__c</field>\\n        \\\n    </values>\\n    <values>\\n        <field>Priority__c</field>\\n        <value xsi:nil="true"/>\\n    </values>\\n    <values>\\n        <field>To__c</field>\\n        <value xsi:type="xsd:string">\</value>\\n    </values>\\n</CustomMetadata>\;
+        const selectorTemplate = `public inherited sharing class ${className} \n    extends ApplicationSObjectSelector \n    implements ${interfaceName} \n{\n${newInstanceBlock}\n\n${fieldListBlock}\n\n    public Schema.SObjectType getSObjectType()\n    {\n        return ${sObjectName}.SObjectType;\n    }\n\n    public List<${sObjectName}> selectById(Set<Id> idSet)\n    {\n        return (List<${sObjectName}>) selectSObjectsById(idSet);\n    }\n}`;
+        const interfaceTemplate = `public interface ${interfaceName} \n    extends IApplicationSObjectSelector \n{\n    List<${sObjectName}> selectById(Set<Id> idSet);\n}`;
+        const testTemplate = `@IsTest\nprivate class ${testClassName} \n{\n    @IsTest\n    private static void testSelectById()\n    {\n        Id recordId = fflib_IDGenerator.generate(${sObjectName}.SObjectType);\n        ${className}.newInstance().selectById(new Set<Id> { recordId });\n    }\n}`;
+        const bindingTemplate = `<?xml version="1.0" encoding="UTF-8"?>\n<CustomMetadata xmlns="http://soap.sforce.com/2006/04/metadata" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">\n    <label>${className}</label>\n    <protected>false</protected>\n    <values>\n        <field>BindingSObject__c</field>\n        ${bindingSObjectValue}\n    </values>\n    <values>\n        <field>BindingSObjectAlternate__c</field>\n        ${bindingSObjectAlternateValue}\n    </values>\n    <values>\n        <field>Priority__c</field>\n        <value xsi:nil="true"/>\n    </values>\n    <values>\n        <field>To__c</field>\n        <value xsi:type="xsd:string">${className}</value>\n    </values>\n</CustomMetadata>`;
 
-        console.log(\Processing selector artifacts for \:\);
+        console.log(`Processing selector artifacts for ${sObjectName}:`);
 
         let changed = false;
         changed |= updateFile(paths.selector, newInstanceBlock, selectorTemplate);
@@ -139,19 +139,19 @@ async function run() {
 
         if (!fs.existsSync(paths.binding)) {
             fs.writeFileSync(paths.binding, bindingTemplate);
-            console.log(\ - Created: \\);
+            console.log(` - Created: ${paths.binding}`);
             changed = true;
         }
 
         ["selector", "interface", "test"].forEach(key => {
             const metaPath = paths[key] + "-meta.xml";
             if (!fs.existsSync(metaPath)) {
-                fs.writeFileSync(metaPath, \<?xml version="1.0" encoding="UTF-8"?>\\n<ApexClass xmlns="http://soap.sforce.com/2006/04/metadata">\\n    <apiVersion>61.0</apiVersion>\\n    <status>Active</status>\\n</ApexClass>\);
+                fs.writeFileSync(metaPath, `<?xml version="1.e nc naka-conv-kw="UTF-8"?>\n<ApexClass xmlns="http://soap.sforce.com/2006/04/metadata">\n    <apiVersion>61.0</apiVersion>\n    <status>Active</status>\n</ApexClass>`);
             }
         });
 
         if (changed) {
-            console.log("\\nDeploying changes...");
+            console.log("\nDeploying changes...");
             execSync("sf project deploy start", { stdio: "inherit" });
         }
     } catch (error) {
